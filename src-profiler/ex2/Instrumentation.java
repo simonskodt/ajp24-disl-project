9aed2e465735464f345745cae159422e57549928
Simon Johann Skødt <simon@skodt.dk>
2025-01-06 12:10:46 +0100

    feat(ex2): finish a (possible) solution, but might need a later change
    Co-authored-by: Francesco De Vito <FraDeVito@users.noreply.github.com>
    Co-authored-by: William Forbrigd <williamforbrigd@users.noreply.github.com>
    Co-authored-by: Luca <luca.airaghi@usi.ch>

--- src-profiler/ex2/Instrumentation.java
+++ src-profiler/ex2/Instrumentation.java
@@ -1,20 +1,58 @@
 package ex2;
 
-import ch.usi.dag.disl.annotation.AfterReturning;
 import ch.usi.dag.disl.annotation.Before;
-import ch.usi.dag.disl.dynamiccontext.DynamicContext;
 import ch.usi.dag.disl.marker.BytecodeMarker;
-import ex1.Profiler;
+
 
 public class Instrumentation {
 
-    @AfterReturning(marker = BytecodeMarker.class, 
-        args="invokestatic, invokespecial, invokevirtual",
-        scope="ex2.MainThread")
-    static void onThreadExit(DynamicContext dynamicContext) {
-        if (dynamicContext.getThis() instanceof Thread) {  
-            // String bytecode = dynamicContext.
-	        // Profiler.atThreadEnd();
-        }
+    // @ThreadLocal
+    // private static long nInvokeStatic;
+
+    // @ThreadLocal
+    // private static long nInvokeSpecial;
+    
+    // @ThreadLocal
+    // private static long nInvokeVirtual;
+
+    // @ThreadLocal
+    // private static long nInvokeDynamic;
+
+    @Before(marker = BytecodeMarker.class, args="invokestatic", scope="ex2.MainThread.*")
+    static void onInvokeStatic() {
+        String threadName = Thread.currentThread().getName();
+        // Profiler.putInThreadMap(threadName, 0);
+        // nInvokeStatic++;
+
+        Profiler.incrementCounter(threadName, "static");
     }
+
+    @Before(marker = BytecodeMarker.class, args="invokespecial", scope="ex2.MainThread.*")
+    static void onInvokeSpecial() {
+        // nInvokeSpecial++;
+        String threadName = Thread.currentThread().getName();
+        Profiler.incrementCounter(threadName, "special");
+    }
+
+    @Before(marker = BytecodeMarker.class, args="invokevirtual", scope="ex2.MainThread.*")
+    static void onInvokeVirtual() {
+        // nInvokeVirtual++;
+        String threadName = Thread.currentThread().getName();
+        Profiler.incrementCounter(threadName, "virtual");
+    }
+
+    @Before(marker = BytecodeMarker.class, args="invokedynamic", scope="ex2.MainThread.*")
+    static void onInvokeDinamic() {
+        // nInvokeDynamic++;
+        String threadName = Thread.currentThread().getName();
+        Profiler.incrementCounter(threadName, "dynamic");
+    }
+
+    // @After(marker = BasicBlockMarker.class, scope="void run()") //guard=IsThreadGuard.class)
+    // static void onThreadExit(DynamicContext dynamicContext) {
+    //     if (dynamicContext.getThis() instanceof Thread) {
+    //         String threadName = Thread.currentThread().getName();
+	//         Profiler.registerThreadEnd(threadName);
+    //     }
+    // }
 }
