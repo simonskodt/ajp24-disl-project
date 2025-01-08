3de1a856a54edb60665b7836f3a1bbe5e2dc780e
FraDeVito <francesco.de.vito@usi.ch>
2025-01-08 15:02:22 +0100

    fix: used tuples and ConcurrentHashMap but partially

--- src-profiler/ex7/Instrumentation.java
+++ src-profiler/ex7/Instrumentation.java
@@ -1,53 +1,33 @@
 package ex7;
 
-import java.util.concurrent.ConcurrentMap;
-
 import ch.usi.dag.disl.annotation.After;
 import ch.usi.dag.disl.annotation.Before;
-import ch.usi.dag.disl.annotation.SyntheticLocal;
 import ch.usi.dag.disl.annotation.ThreadLocal;
 import ch.usi.dag.disl.dynamiccontext.DynamicContext;
-import ch.usi.dag.disl.marker.BasicBlockMarker;
 import ch.usi.dag.disl.marker.BytecodeMarker;
-import ch.usi.dag.disl.staticcontext.BasicBlockStaticContext;
-import ch.usi.dag.disl.staticcontext.MethodStaticContext;
 
 public class Instrumentation {
     
-    // Update to include the whole runtime
+    // Update to include all methods
     private static final String SCOPE = "ex7.MainThread.*";
 
     @ThreadLocal
-    private static int[] arrLength;
+    private static int arrLength;
 
     @ThreadLocal
-    private static String[] componentType;
+    private static String componentType;
 
-    // @SyntheticLocal
-    // ConcurrentMap<
-    
     @Before(marker=BytecodeMarker.class , args="newarray, anewarray", scope=SCOPE)
-    static void findArrayLength(DynamicContext dynamicContext, BasicBlockStaticContext basicBlockStaticContext) {
-        int stackTop = dynamicContext.getStackValue(0, int.class);
-        // arrLength
-        basicBlockStaticContext.
-        // System.err.println("Length: " + arrLength);
-
+    static void findArrayLength(DynamicContext dynamicContext) {
+        arrLength = dynamicContext.getStackValue(0, int.class);
     }
 
     @After(marker=BytecodeMarker.class, args="newarray, anewarray", scope=SCOPE)
     static void findArrayType(DynamicContext dynamicContext) {
-        // if (dynamicContext.getStackValue(0, int.class)) {
-        //     int componentType = 
-        // }
-        componentType = (String) dc.getStackValue(0, Object.class).getClass().getName();
-        System.err.println("Array type: " + componentType);
-    }
-
-    @After(marker = BasicBlockMarker.class, scope="ex7.MainThread.run")
-    static void onThreadExit(DynamicContext dynamicContext) {
-        if (dynamicContext.getThis() instanceof Thread) {  
-            Profiler.registerThreadEnd();
+        Object array = dynamicContext.getStackValue(0, Object.class);
+        if (array != null && array.getClass().isArray()) {
+            componentType = array.getClass().getComponentType().getName();
+            Profiler.registerArrayAllocation(componentType, arrLength);
         }
-    } 
+    }
 }
--- src-profiler/ex7/Profiler.java
+++ src-profiler/ex7/Profiler.java
@@ -1,7 +1,9 @@
 package ex7;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Comparator;
+
 
 public class Profiler {
 
@@ -14,6 +16,23 @@ public class Profiler {
             this.x = x;
             this.y = y;
         }
+
+        @Override
+        public int hashCode() {
+            return x.hashCode() ^ y.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Tuple)) return false;
+            Tuple tuple = (Tuple) o;
+            return this.x.equals(tuple.x) && this.y.equals(tuple.y);
+        }
+
+        @Override
+        public String toString() {
+            return x + ", " + y;
+        }
     }
 
     static {
@@ -21,31 +40,32 @@ public class Profiler {
     }
 
     static class ShutdownThread extends Thread {
-	
         public void run() {
-            for (var entry : allocations.entrySet()) {
-                Tuple<String, Integer> key = entry.getKey();
-                
-                System.err.format("%s, %d: %d allocations\n", 
-                    key.x, key.y, entry.getValue()
-                );
-            }
+            allocations.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(new Comparator<Tuple<String, Integer>>() {
+                    @Override
+                    public int compare(Tuple<String, Integer> o1, Tuple<String, Integer> o2) {
+                        int cmp = o1.x.compareTo(o2.x);
+                        if (cmp != 0) {
+                            return cmp;
+                        }
+                        return o1.y.compareTo(o2.y);
+                    }
+                }))
+                .forEach(entry -> {
+                    Tuple<String, Integer> key = entry.getKey();
+                    System.err.format("%s, %d: %d allocations\n", key.x, key.y, entry.getValue());
+                });
         }
     }
 
+    public static void registerArrayAllocation(String componentType, int length) {
+        Tuple<String, Integer> key = new Tuple<>(componentType, length);
+        allocations.putIfAbsent(key, 0);
+        allocations.computeIfPresent(key, (k, v) -> v + 1);
+    }
+
     public static void registerThreadEnd() {
-        try {
-            Field comp = Thread.class.getDeclaredField("componentType");
-            String component = (String) comp.get(Thread.currentThread());
-
-            Field l = Thread.class.getDeclaredField("arrLength");
-            Integer arrLength = l.getInt(Thread.currentThread());
-
-            Tuple<String, Integer> key = new Tuple<>(component, arrLength);
-            allocations.putIfAbsent(key, 1);
-            allocations.computeIfPresent(key, (k, v) -> v + 1);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        // No additional actions needed for thread end in this context
     }
 }
