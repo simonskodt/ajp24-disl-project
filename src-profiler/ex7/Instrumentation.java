03249caa39efbba2ac262447ab37206dd3d6b921
Simon Johann Skødt <simon@skodt.dk>
2025-01-08 14:32:21 +0100

    feat: with tuple
    Co-authored-by: William Forbrigd <williamforbrigd@users.noreply.github.com>
    Co-authored-by: Francesco De Vito <FraDeVito@users.noreply.github.com>

--- src-profiler/ex7/Instrumentation.java
+++ src-profiler/ex7/Instrumentation.java
@@ -1,53 +1,29 @@
 package ex7;
 
-import java.util.concurrent.ConcurrentMap;
+import java.lang.reflect.Array;
 
 import ch.usi.dag.disl.annotation.After;
-import ch.usi.dag.disl.annotation.Before;
-import ch.usi.dag.disl.annotation.SyntheticLocal;
-import ch.usi.dag.disl.annotation.ThreadLocal;
 import ch.usi.dag.disl.dynamiccontext.DynamicContext;
-import ch.usi.dag.disl.marker.BasicBlockMarker;
 import ch.usi.dag.disl.marker.BytecodeMarker;
-import ch.usi.dag.disl.staticcontext.BasicBlockStaticContext;
-import ch.usi.dag.disl.staticcontext.MethodStaticContext;
 
 public class Instrumentation {
     
-    // Update to include the whole runtime
     private static final String SCOPE = "ex7.MainThread.*";
-
-    @ThreadLocal
-    private static int[] arrLength;
-
-    @ThreadLocal
-    private static String[] componentType;
-
-    // @SyntheticLocal
-    // ConcurrentMap<
-    
-    @Before(marker=BytecodeMarker.class , args="newarray, anewarray", scope=SCOPE)
-    static void findArrayLength(DynamicContext dynamicContext, BasicBlockStaticContext basicBlockStaticContext) {
-        int stackTop = dynamicContext.getStackValue(0, int.class);
-        // arrLength
-        basicBlockStaticContext.
-        // System.err.println("Length: " + arrLength);
-
-    }
 
     @After(marker=BytecodeMarker.class, args="newarray, anewarray", scope=SCOPE)
     static void findArrayType(DynamicContext dynamicContext) {
-        // if (dynamicContext.getStackValue(0, int.class)) {
-        //     int componentType = 
-        // }
-        componentType = (String) dc.getStackValue(0, Object.class).getClass().getName();
-        System.err.println("Array type: " + componentType);
-    }
+        Object obj = dynamicContext.getStackValue(0, Object.class);
 
-    @After(marker = BasicBlockMarker.class, scope="ex7.MainThread.run")
-    static void onThreadExit(DynamicContext dynamicContext) {
-        if (dynamicContext.getThis() instanceof Thread) {  
-            Profiler.registerThreadEnd();
-        }
-    } 
+        String component = obj.getClass().getName();
+        int length = Array.getLength(obj);
+
+        // System.err.println(component + ", " + length);
+
+        // Profiler.Tuple key = new Profiler.Tuple(component, length);
+        String key = component + ", " + length;
+        Profiler.allocations.putIfAbsent(key, 1);
+        Profiler.allocations.put(key, Profiler.allocations.get(key)+ 1);
+
+        // Profiler.allocations.computeIfPresent(key, (k, v) -> v + 1);
+    }
 }
--- src-profiler/ex7/Profiler.java
+++ src-profiler/ex7/Profiler.java
@@ -1,18 +1,34 @@
 package ex7;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.lang.reflect.Field;
 
 public class Profiler {
 
-    private static ConcurrentHashMap<Tuple<String, Integer>, Integer> allocations = new ConcurrentHashMap<>();
+    // public static ConcurrentHashMap<Tuple, Integer> allocations = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<String, Integer> allocations = new ConcurrentHashMap<>();
 
-    public static class Tuple<X, Y> {
-        final X x;
-        final Y y;
-        public Tuple(X x, Y y) {
+    public static class Tuple {
+        public final String x;
+        public final int y;
+
+        public Tuple(String x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return x + ", " + y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Tuple)) {
+                return false;
+            }
+
+            Tuple t = (Tuple) o;
+            return this.x.equals(t.x) && this.y == t.y;
         }
     }
 
@@ -24,28 +40,15 @@ public class Profiler {
 	
         public void run() {
             for (var entry : allocations.entrySet()) {
-                Tuple<String, Integer> key = entry.getKey();
+                // Tuple key = entry.getKey();
+                String key = entry.getKey();
                 
-                System.err.format("%s, %d: %d allocations\n", 
-                    key.x, key.y, entry.getValue()
-                );
+                // System.err.format("%s, %d: %d allocations\n", 
+                //     key.x, key.y, entry.getValue()
+                // );
+                System.err.format("%s: %d allocations\n",
+                key, entry.getValue());
             }
-        }
-    }
-
-    public static void registerThreadEnd() {
-        try {
-            Field comp = Thread.class.getDeclaredField("componentType");
-            String component = (String) comp.get(Thread.currentThread());
-
-            Field l = Thread.class.getDeclaredField("arrLength");
-            Integer arrLength = l.getInt(Thread.currentThread());
-
-            Tuple<String, Integer> key = new Tuple<>(component, arrLength);
-            allocations.putIfAbsent(key, 1);
-            allocations.computeIfPresent(key, (k, v) -> v + 1);
-        } catch(Exception e) {
-            e.printStackTrace();
         }
     }
 }
